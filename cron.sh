cb600035cc250c56aa3d99c0c5c8a968bbe35866
hcslearning <53094205+hcslearning@users.noreply.github.com>
2019-09-20 22:23:15 -0300

    Update cron.sh
    crontab -e

--- cron.sh
+++ cron.sh
@@ -1,3 +1,4 @@
+# crontab -e
 # minutos (0-59), horas (0-23), día del mes (1-31), mes (1-12) y día de la semana (0-6, 0=domingo)
 # todos los meses el día 1ero a las 2am
 0 2 1 * * rsync -avz /ParaRespaldar/Comun /Respaldo/mensual
